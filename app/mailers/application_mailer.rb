f1777a1f143362446a3947974c3c124f9dd24644
JoseMZapata <josemanuelzapaatarangel@gmail.com>
2025-10-21 12:53:19 -0600

    sadasd3w

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "josemanuelzapaatarangel@gmail.com"
+  before_action :set_xouath2_access_token
+  default from: "example.com"
   layout "mailer"
 end
